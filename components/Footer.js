8cb0c875b5bde3f1ca5ad05346e89d3e93e8b1e9
Himanshu__808 <55khimanshu@gmail.com>
2025-05-22 19:48:41 +0530

    Implement layout structure with Navbar and Footer, add Clerk authentication middleware, and create sign-in page

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-yellow-950 text-white flex justify-center px-4 items-center h-16'>
+      <p className='text-center'>Copyright &copy; Get me a Coffee - All rights reserved!</p>
+
+    </footer>
+  )
+}
+
+export default Footer
