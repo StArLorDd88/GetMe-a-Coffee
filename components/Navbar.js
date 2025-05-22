fd2d3223a03c515b22eca94fee4e46445ba3066e
Himanshu__808 <55khimanshu@gmail.com>
2025-05-22 20:19:26 +0530

    Fix Navbar link to point to sign-in page and enhance Clerk middleware for better route protection

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className='bg-yellow-950 text-white flex justify-between px-4 items-center h-16'>
       <div className="logo font-bold">GetMe a Coffee!</div>
       <div>
-        <Link href={"/login"}>
+        <Link href={"/sign-in"}>
         <button className="bg-amber-600 text-lg text-black font-extrabold font-sans px-5 py-2 border-2 border-black rounded-md shadow-sm cursor-pointer hover:-translate-x-0.5 hover:-translate-y-0.5 hover:shadow active:translate-x-0.5 active:translate-y-0.5 active:shadow-xs transition-transform">
           Login
         </button></Link>
--- middleware/clerk.js
+++ middleware/clerk.js
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in',
@@ -8,10 +9,30 @@ const isPublicRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+    const { userId } = auth()
+    const currentUrl = new URL(req.url)
+    const isAccessingDashboard = currentUrl.pathname == "/home"
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+    if(userId && isPublicRoute(req) && !isAccessingDashboard){
+      return NextResponse.redirect(new URL("/home", req.URL))
+    }
+
+    //not loggedin
+    if(!userId){
+      //if user is not logged in and trying to access a protected route
+      if(!isPublicRoute(req)){
+        return NextResponse.redirect(new URL("/sign-in", req.URL))
+      }
+
+      //if the request is for a protected API and the user is not logged in
+      if(isApiRequest){
+        return NextResponse.redirect(new URL("/sign_in", req.url))
+      }
+    }
+    return NextResponse.next()
   }
-})
+)
 
 export const config = {
   matcher: [
